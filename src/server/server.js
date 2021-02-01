1f539a281197b88c4eff40c78a71e9a235f15a58
kkupcin <kkupcin@gmail.com>
2021-02-01 18:17:02 +0000

    setup

---
+++ src/server/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fetch = require('node-fetch')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+
+// Starting instance of an app
+const app = express()
+app.use(cors())
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder (dist for Webpack)
+app.use(express.static('dist'));
+
+// ASK ZIL
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// Setup Server
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// ROUTES
+
+app.get('/getData', function (req, res) {
+    res.send(projectData);
+});
+
+app.post('/postData', function (req, res) {
+    projectData = req.body;
+    res.send('Post successful');
+});
